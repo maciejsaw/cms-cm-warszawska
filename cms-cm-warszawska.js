02023f056c846fd2c6f1b04f9d81f1bc4615d649
Maciej Sawicki <saw.maciej@gmail.com>
2018-02-17 11:24:17 +0100

    Update cms-cm-warszawska.js

--- cms-cm-warszawska.js
+++ cms-cm-warszawska.js
@@ -1,6 +1,10 @@
 var cmsTexts = {
   
-  numerTelefonu: "+45 343 343",
+  numerTelefonu: "85 875 53 55",
+  aktualnoscPierwszaTytul: "Specjalista medycy rodzinnej, specjalista chorób wewnętrznych (internista), lekarz pediatra - wizyty prywatne",
+  aktualnoscPierwszaData: "Czwartek, 14 lutego 2018",
+  aktualnoscPierwszaTekst: "Prosimy o rejestrację telefoniczną",
+  
   
   
 }
