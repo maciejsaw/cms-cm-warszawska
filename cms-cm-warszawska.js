7913a98ae9f9db19d35220766298940c78376889
Maciej Sawicki <saw.maciej@gmail.com>
2018-02-17 11:35:57 +0100

    Update cms-cm-warszawska.js

--- cms-cm-warszawska.js
+++ cms-cm-warszawska.js
@@ -20,5 +20,16 @@ var cmsTexts = {
   aktualnoscDrugaTekst: 
   "Zabiegi wykonywane są przez doświadczonego lekarza kosmetologa z 20-letnią praktyką.",
   
+  dodatkoweKontaktyAkapitPierwszy:
+  "Poza godzinami pracy poradni, bezpłatną opiekę nad naszymi pacjentami przejmuje SPZOZ Wojewódzka Stacja Pogotowia Ratunkowego przy ul. Poleskiej 89  | tel  85 652 67 14",
+  
+  dodatkoweKontaktyAkapitDrugi:
+  "Współpracujemy z Centrum Położnych Środowiskowych Sp.p. Położna środowiskowa Nina Sobiecka: tel 604 564 843",
+  
+  dodatkoweKontaktyAkapitTrzeci:
+  "Informacja medyczne całodobowo | tel. 194 39",
+  
+  
+  
   
 }
