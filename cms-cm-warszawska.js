6300539c48d338e9713cb1586bf0e2ec57d1a61b
Maciej Sawicki <saw.maciej@gmail.com>
2018-02-17 13:01:38 +0100

    Update cms-cm-warszawska.js

--- cms-cm-warszawska.js
+++ cms-cm-warszawska.js
@@ -1,109 +1,109 @@
 var cmsTexts = {
   
   numerTelefonu: 
-  "X", //"85 875 53 55",
+  "85 875 53 55",
   
   numerTelefonuBezSpacji: 
-  "X", //"858755355",
+  "858755355",
   
   numerTelefonuKomorka: 
-  "X", //"+48 512 677 311",
+  "+48 512 677 311",
   
   numerTelefonuKomorkaBezSpacji: 
-  "X", //"+48512677311",
+  "+48512677311",
   
   numerTelefonuMedstyl: 
-  "X", //"662 149 977",
+  "662 149 977",
   
   numerTelefonuMedstylBezSpacji: 
-  "X", //"662149977",
+  "662149977",
   
   aktualnoscPierwszaTytul: 
-  "X", //Specjalista medycy rodzinnej, specjalista chorób wewnętrznych (internista), lekarz pediatra - wizyty prywatne",
+  "Specjalista medycy rodzinnej, specjalista chorób wewnętrznych (internista), lekarz pediatra - wizyty prywatne",
   
   aktualnoscPierwszaData: 
-  "X", //Czwartek, 14 lutego 2018",
+  "Czwartek, 14 lutego 2018",
   
   aktualnoscPierwszaTekst: 
-  "X", //Prosimy o rejestrację telefoniczną",
+  "Prosimy o rejestrację telefoniczną",
   
   aktualnoscDrugaTytul: 
-  "X", //Promocja! Powiększanie ust - super cena od 600zł",
+  "Promocja! Powiększanie ust - super cena od 600zł",
   
   aktualnoscDrugaData: 
-  "X", //Wtorek, 14 lutego 2017",
+  "Wtorek, 14 lutego 2017",
   
   aktualnoscDrugaTekst: 
-  "X", //Zabiegi wykonywane są przez doświadczonego lekarza kosmetologa z 20-letnią praktyką.",
+  "Zabiegi wykonywane są przez doświadczonego lekarza kosmetologa z 20-letnią praktyką.",
   
   dodatkoweKontaktyAkapitPierwszy:
-  "X", //Poza godzinami pracy poradni, bezpłatną opiekę nad naszymi pacjentami przejmuje SPZOZ Wojewódzka Stacja Pogotowia Ratunkowego przy ul. Poleskiej 89  | tel  85 652 67 14",
+  "Poza godzinami pracy poradni, bezpłatną opiekę nad naszymi pacjentami przejmuje SPZOZ Wojewódzka Stacja Pogotowia Ratunkowego przy ul. Poleskiej 89  | tel  85 652 67 14",
   
   dodatkoweKontaktyAkapitDrugi:
-  "X", //"Współpracujemy z Centrum Położnych Środowiskowych Sp.p. Położna środowiskowa Nina Sobiecka: tel 604 564 843",
+  "Współpracujemy z Centrum Położnych Środowiskowych Sp.p. Położna środowiskowa Nina Sobiecka: tel 604 564 843",
   
   dodatkoweKontaktyAkapitTrzeci:
-  "X", //"Informacja medyczne całodobowo | tel. 194 39",
+  "Informacja medyczne całodobowo | tel. 194 39",
   
   kontaktGodzinyOtwarcia: 
-  "X", //"PN - PT  w godzinach 8.00 - 18.00",
+  "PN - PT  w godzinach 8.00 - 18.00",
   
   rodzinnaGodzinyOtwarcia:
-   "X", //"Poradnia jest czynna od poniedziałku do piątku w godzinach 8.00 - 18.00. Zapisy na wizyty w godz. 8.00- 17.00 telefonicznie, osobiście, za pośrednictwem osób trzecich.",
+  "Poradnia jest czynna od poniedziałku do piątku w godzinach 8.00 - 18.00. Zapisy na wizyty w godz. 8.00- 17.00 telefonicznie, osobiście, za pośrednictwem osób trzecich.",
   
   rodzinnaLekarz1:
-  "X", //"lek. med. Elżbieta Malinowska - specjalista medycyny rodzinnej, pediatra",
+  "lek. med. Elżbieta Malinowska - specjalista medycyny rodzinnej, pediatra",
   
   rodzinnaLekarz2:
-  "X", //"lek. med. Patrycja Budna - specjalista medycyny rodzinnej",
+  "lek. med. Patrycja Budna - specjalista medycyny rodzinnej",
   
   rodzinnaLekarz3:
-  "X", //"lek. med. Wojciech Budny - specjalista chorób wewnętrznych",
+  "lek. med. Wojciech Budny - specjalista chorób wewnętrznych",
   
   rodzinnaLekarz4:
-  "X", //"piel. Irena Dawidowicz - specjalista pielęgniarstwa pediatrycznego",
+  "piel. Irena Dawidowicz - specjalista pielęgniarstwa pediatrycznego",
   
   rodzinnaLekarz5:
-  "X", //"mgr piel. Urszula Moniuszko - specjalista pielęgniarstwa anestezjologicznego i intensywnej opieki",
+  "mgr piel. Urszula Moniuszko - specjalista pielęgniarstwa anestezjologicznego i intensywnej opieki",
   
   rodzinnaLekarz6:
-  "X", //"piel. Katarzyna Jasińska",
+  "piel. Katarzyna Jasińska",
   
   rodzinnaLekarz7:
-  "X", //"mgr położna Katarzyna Kitlasz - Baczyńska",
+  "mgr położna Katarzyna Kitlasz - Baczyńska",
   
   rodzinnaLekarz8:
-  "X",
+  "",
   
   rodzinnaLekarz9:
   "",
   
   personelRamkaOpiekaWSwieta: 
-  "X", //"W godzinach 18.00 - 8.00 od poniedziałku do piątku, w weekendy oraz w dni wolne ustawowo i świąteczne bezpłatną opiekę nad naszymi pacjentami przejmuje Samodzielny Szpital Miejski im. PCK ul.Sienkiewicza 79 w Białymstoku tel. 85 652 25 05 oraz SPZOZ Wojewódzki Szpital Zespolony ul. Skłodowskiej 26 w Białymstoku tel. 85 748 81 26",
+  "W godzinach 18.00 - 8.00 od poniedziałku do piątku, w weekendy oraz w dni wolne ustawowo i świąteczne bezpłatną opiekę nad naszymi pacjentami przejmuje Samodzielny Szpital Miejski im. PCK ul.Sienkiewicza 79 w Białymstoku tel. 85 652 25 05 oraz SPZOZ Wojewódzki Szpital Zespolony ul. Skłodowskiej 26 w Białymstoku tel. 85 748 81 26",
   
   usgLekarz1__imie:
-  "X", //"Lek. med. Marta Martynowicz",
+  "Lek. med. Marta Martynowicz",
   
   usgLekarz1__opis:
-  "X", //"Badania niemowląt, dzieci i dorosłych",
+  "Badania niemowląt, dzieci i dorosłych",
   
   usgLekarz1__godziny:
   "",
   
   usgLekarz2__imie:
-  "X", //"Lek. med. Monika Plona-Herbst",
+  "Lek. med. Monika Plona-Herbst",
   
   usgLekarz2__opis:
-  "X", //"Badania niemowląt, dzieci i dorosłych",
+  "Badania niemowląt, dzieci i dorosłych",
   
   usgLekarz2__godziny:
   "",
   
   usgLekarz3__imie:
-  "X", //"Lek. med. Julia Rutkowska",
+  "Lek. med. Julia Rutkowska",
   
   usgLekarz3__opis:
-  "X", //"Specjalista radiologii i diagnostyki obrazowej. Badania dzieci i dorosłych",
+  "Specjalista radiologii i diagnostyki obrazowej. Badania dzieci i dorosłych",
   
   usgLekarz3__godziny:
   "",
