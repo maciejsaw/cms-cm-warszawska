78d81cb7aac83315fab00048cc95cbd2c2d7f278
Maciej Sawicki <saw.maciej@gmail.com>
2018-02-17 11:27:45 +0100

    Update cms-cm-warszawska.js

--- cms-cm-warszawska.js
+++ cms-cm-warszawska.js
@@ -1,10 +1,24 @@
 var cmsTexts = {
   
   numerTelefonu: "85 875 53 55",
-  aktualnoscPierwszaTytul: "Specjalista medycy rodzinnej, specjalista chorób wewnętrznych (internista), lekarz pediatra - wizyty prywatne",
-  aktualnoscPierwszaData: "Czwartek, 14 lutego 2018",
-  aktualnoscPierwszaTekst: "Prosimy o rejestrację telefoniczną",
   
+  aktualnoscPierwszaTytul: 
+  "Specjalista medycy rodzinnej, specjalista chorób wewnętrznych (internista), lekarz pediatra - wizyty prywatne",
+  
+  aktualnoscPierwszaData: 
+  "Czwartek, 14 lutego 2018",
+  
+  aktualnoscPierwszaTekst: 
+  "Prosimy o rejestrację telefoniczną",
+  
+  aktualnoscDrugaTytul: 
+  "Promocja! Powiększanie ust - super cena od 600zł",
+  
+  aktualnoscDrugaData: 
+  "Wtorek, 14 lutego 2017",
+  
+  aktualnoscDrugaTekst: 
+  "Zabiegi wykonywane są przez doświadczonego lekarza kosmetologa z 20-letnią praktyką.",
   
   
 }
