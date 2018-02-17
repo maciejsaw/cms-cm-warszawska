f444f7698721f980127640780219cafcf66228ef
Maciej Sawicki <saw.maciej@gmail.com>
2018-02-17 12:05:36 +0100

    Update cms-cm-warszawska.js

--- cms-cm-warszawska.js
+++ cms-cm-warszawska.js
@@ -57,7 +57,137 @@ var cmsTexts = {
   rodzinnaLekarz2:
   "X", //"lek. med. Patrycja Budna - specjalista medycyny rodzinnej",
   
+  rodzinnaLekarz3:
+  "X", //"lek. med. Wojciech Budny - specjalista chorób wewnętrznych",
   
+  rodzinnaLekarz4:
+  "X", //"piel. Irena Dawidowicz - specjalista pielęgniarstwa pediatrycznego",
+  
+  rodzinnaLekarz5:
+  "X", //"mgr piel. Urszula Moniuszko - specjalista pielęgniarstwa anestezjologicznego i intensywnej opieki",
+  
+  rodzinnaLekarz6:
+  "X", //"piel. Katarzyna Jasińska",
+  
+  rodzinnaLekarz7:
+  "X", //"mgr położna Katarzyna Kitlasz - Baczyńska",
+  
+  rodzinnaLekarz8:
+  "",
+  
+  rodzinnaLekarz9:
+  "",
+  
+  personelRamkaOpiekaWSwieta: 
+  "X", //"W godzinach 18.00 - 8.00 od poniedziałku do piątku, w weekendy oraz w dni wolne ustawowo i świąteczne bezpłatną opiekę nad naszymi pacjentami przejmuje Samodzielny Szpital Miejski im. PCK ul.Sienkiewicza 79 w Białymstoku tel. 85 652 25 05 oraz SPZOZ Wojewódzki Szpital Zespolony ul. Skłodowskiej 26 w Białymstoku tel. 85 748 81 26",
+  
+  usgLekarz1__imie:
+  "X", //"Lek. med. Marta Martynowicz",
+  
+  usgLekarz1__opis:
+  "X", //"Badania niemowląt, dzieci i dorosłych",
+  
+  usgLekarz2__imie:
+  "X", //"Lek. med. Monika Plona-Herbst",
+  
+  usgLekarz2__opis:
+  "X", //"Badania niemowląt, dzieci i dorosłych",
+  
+  usgLekarz3__imie:
+  "X", //"Lek. med. Julia Rutkowska",
+  
+  usgLekarz3__opis:
+  "X", //"Specjalista radiologii i diagnostyki obrazowej. Badania dzieci i dorosłych",
+  
+  usgLekarz4__imie:
+  "",
+  
+  usgLekarz4__opis:
+  "",
+  
+  usgCennik__wiersz1__tytul:
+  "USG brzucha",
+  
+  usgCennik__wiersz1__cena:
+  "70 zł",
+  
+  usgCennik__wiersz2__tytul:
+  "",
+  
+  usgCennik__wiersz2__cena:
+  "",
+  
+  usgCennik__wiersz3__tytul:
+  "",
+  
+  usgCennik__wiersz3__cena:
+  "",
+  
+  usgCennik__wiersz4__tytul:
+  "",
+  
+  usgCennik__wiersz4__cena:
+  "",
+  
+  usgCennik__wiersz5__tytul:
+  "",
+  
+  usgCennik__wiersz5__cena:
+  "",
+  
+  usgCennik__wiersz6__tytul:
+  "",
+  
+  usgCennik__wiersz6__cena:
+  "",
+  
+  usgCennik__wiersz7__tytul:
+  "",
+  
+  usgCennik__wiersz7__cena:
+  "",
+  
+  usgCennik__wiersz8__tytul:
+  "",
+  
+  usgCennik__wiersz8__cena:
+  "",
+  
+  usgCennik__wiersz9__tytul:
+  "",
+  
+  usgCennik__wiersz9__cena:
+  "",
+  
+  usgCennik__wiersz10__tytul:
+  "",
+  
+  usgCennik__wiersz11__cena:
+  "",
+  
+  usgCennik__wiersz12__tytul:
+  "",
+  
+  usgCennik__wiersz12__cena:
+  "",
+  
+  usgCennik__wiersz13__tytul:
+  "",
+  
+  usgCennik__wiersz13__cena:
+  "",
+  
+  usgCennik__wiersz14__tytul:
+  "",
+  
+  usgCennik__wiersz14__cena:
+  "",
+  
+  usgCennik__wiersz15__tytul:
+  "",
+  
+  usgCennik__wiersz15__cena:
+  "",  
   
   
 }
