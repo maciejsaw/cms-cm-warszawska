4f5d5af724fb3d25e71e881a10a83c94a99b856e
Maciej Sawicki <saw.maciej@gmail.com>
2018-02-17 11:38:44 +0100

    Update cms-cm-warszawska.js

--- cms-cm-warszawska.js
+++ cms-cm-warszawska.js
@@ -1,33 +1,34 @@
 var cmsTexts = {
   
-  numerTelefonu: "85 875 53 55",
+  numerTelefonu: 
+  "X", //"85 875 53 55",
   
   aktualnoscPierwszaTytul: 
-  "Specjalista medycy rodzinnej, specjalista chorób wewnętrznych (internista), lekarz pediatra - wizyty prywatne",
+  "X", //Specjalista medycy rodzinnej, specjalista chorób wewnętrznych (internista), lekarz pediatra - wizyty prywatne",
   
   aktualnoscPierwszaData: 
-  "Czwartek, 14 lutego 2018",
+  "X", //Czwartek, 14 lutego 2018",
   
   aktualnoscPierwszaTekst: 
-  "Prosimy o rejestrację telefoniczną",
+  "X", //Prosimy o rejestrację telefoniczną",
   
   aktualnoscDrugaTytul: 
-  "Promocja! Powiększanie ust - super cena od 600zł",
+  "X", //Promocja! Powiększanie ust - super cena od 600zł",
   
   aktualnoscDrugaData: 
-  "Wtorek, 14 lutego 2017",
+  "X", //Wtorek, 14 lutego 2017",
   
   aktualnoscDrugaTekst: 
-  "Zabiegi wykonywane są przez doświadczonego lekarza kosmetologa z 20-letnią praktyką.",
+  "X", //Zabiegi wykonywane są przez doświadczonego lekarza kosmetologa z 20-letnią praktyką.",
   
   dodatkoweKontaktyAkapitPierwszy:
-  "Poza godzinami pracy poradni, bezpłatną opiekę nad naszymi pacjentami przejmuje SPZOZ Wojewódzka Stacja Pogotowia Ratunkowego przy ul. Poleskiej 89  | tel  85 652 67 14",
+  "X", //Poza godzinami pracy poradni, bezpłatną opiekę nad naszymi pacjentami przejmuje SPZOZ Wojewódzka Stacja Pogotowia Ratunkowego przy ul. Poleskiej 89  | tel  85 652 67 14",
   
   dodatkoweKontaktyAkapitDrugi:
-  "Współpracujemy z Centrum Położnych Środowiskowych Sp.p. Położna środowiskowa Nina Sobiecka: tel 604 564 843",
+  "X", //"Współpracujemy z Centrum Położnych Środowiskowych Sp.p. Położna środowiskowa Nina Sobiecka: tel 604 564 843",
   
   dodatkoweKontaktyAkapitTrzeci:
-  "Informacja medyczne całodobowo | tel. 194 39",
+  "X", //"Informacja medyczne całodobowo | tel. 194 39",
   
   
   
